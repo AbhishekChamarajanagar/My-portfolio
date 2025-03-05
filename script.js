9e16d6d330aa04352b0b29fe6eab3339873789ff
AbhishekChamarajanagar <abhisheknayakaks2341@gmail.com>
2025-03-05 17:19:49 +0530

    edited

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
   const body = document.body;
 
+//   const skills = [
+//     { name: "Git", icon: "./assets/icon-git.svg" },
+//     { name: "JavaScript", icon: "./assets/icon-javscript.svg" },
+//     { name: "Css", icon: "./assets/css.svg" },
+//     { name: "Html", icon: "./assets/html.svg" }
+// ];
+// const skill_list = document.getElementById("skill-list");
+
+
+
+// skills.forEach(skill => {
+//   const skillCard = document.createElement("div");
+
+//   skillCard.innerHTML = `
+//       <img src="${skill.icon}" alt="${skill.name}">
+//       <p>${skill.name}</p>
+//   `;
+
+//   skill_list.appendChild(skillCard);
+// });
   // Check for saved theme
   if (localStorage.getItem('theme') === 'dark') {
     body.classList.add('dark-mode');
